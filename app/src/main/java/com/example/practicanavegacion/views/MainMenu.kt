7d8715e6771cf234865dfd4cf5d75c665a2a89e2
CharlieLemonade <carloslopzt15@gmail.com>
2024-10-24 20:36:22 -0600

    Combine Años Perrunos, Descuesto App, and Lotería

---
+++ app/src/main/java/com/example/practicanavegacion/views/MainMenu.kt
@@ -0,0 +1,127 @@
+package com.example.practicanavegacion.views
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.practicanavegacion.components.MainButton
+import com.example.practicanavegacion.components.TitleBar
+import com.example.practicanavegacion.components.Space
+import com.example.practicanavegacion.components.TitleView
+import com.example.practicanavegacion.components.MainIconButton
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun Menu(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        TitleBar(name = "HOME PAGE")
+                    }
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFFFB445B)
+                )
+            )
+        }
+    ) { paddingValues ->
+        ContentMenuView(navController, paddingValues)
+    }
+}
+
+@Composable
+fun ContentMenuView(navController: NavController, paddingValues: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TitleView(name = "Menú", fontSize = 32.sp)
+
+
+        val menuItems = listOf("Descuentos", "DogYear", "Loteria")
+
+
+        menuItems.forEach { item ->
+            MenuButton(name = item, navController)
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun MenuButton(name: String, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .clip(RoundedCornerShape(12.dp)),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFFB445B))
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { navController.navigate(name) },
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = name,
+                color = Color.White,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun TitleView(name: String, fontSize: TextUnit) {
+    Text(
+        text = name,
+        fontSize = fontSize,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFFFB445B),
+        modifier = Modifier.padding(bottom = 16.dp)
+    )
+}
